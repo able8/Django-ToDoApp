3353ad6ab72db194f5c6e2b640de2bf920ca6c3a
able8 <able8@me.com>
2018-08-16 11:26:02 +0800

    part18 home page use db

--- to_do_list/todolist/views.py
+++ to_do_list/todolist/views.py
@@ -1,33 +1,29 @@
 from django.shortcuts import render, redirect
-
+from .models import Todo
 # Create your views here.
 
-lst = [
-    {'待办事项':'遛狗', '已完成': False},
-    {'待办事项':'发快递', '已完成': True},
-]
 
 
 def home(request):
     # request.POST
-    global lst
     if request.method == 'POST':
         if request.POST['待办事项'] == '':
-            content = {'清单': lst, '警告': '请收入内容！'}
+            content = {'清单': Todo.objects.all(), '警告': '请收入内容！'}
             return render(request, 'todolist/home.html', content) 
         else:
-            lst.append({'待办事项':request.POST['待办事项'], '已完成': False})
-            content = {'清单': lst, '信息': '添加成功！'}
-            return render(request, 'todolist/home.html', content) 
+            # lst.append({'待办事项':request.POST['待办事项'], '已完成': False})
+            a_row = Todo(thing=request.POST['待办事项'], done=False)
+            a_row.save()
+            content = {'清单': Todo.objects.all(), '信息': '添加成功！'}
+            return render(request, 'todolist/home.html', content)
     elif request.method == 'GET':
-        content = {'清单': lst}
+        content = {'清单': Todo.objects.all()}
         return render(request, 'todolist/home.html', content)
 
 def about(request):
     return render(request, 'todolist/about.html')
 
 def edit(request, forloop_counter):
-    global lst
     if request.method == 'POST': 
         if request.POST['已修改事项'] == '':
            return render(request, 'todolist/edit.html', {'警告': '请收入内容！'})
@@ -44,7 +40,6 @@ def delete(request, forloop_counter):
     return redirect('todolist:主页')
 
 def cross(request, forloop_counter):
-    global lst
     if request.POST['完成状态'] == '已完成':
         lst[int(forloop_counter) - 1]['已完成'] = True
         return redirect('todolist:主页')
